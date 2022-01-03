8f8ac5e7218abe03a333b71ac85887802571294b
MiltonVasquezEndava <milton.vasquez@endava.com>
2022-01-03 09:26:51 -0500

    Initial project to learn .netCore

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,35 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMeryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMeryRestaurantData()
+        {
+            restaurants = new List<Restaurant>() {
+                new Restaurant { Id = 1, Name = "Milton Pizza",Location = "Colombia", Cuisine = CuisineType.Mexican },
+                new Restaurant { Id = 2, Name = "Milton hamburguer", Location = "Henderson", Cuisine = CuisineType.Indian },
+                new Restaurant { Id = 3, Name = "Milton patapas", Location = "EEUU", Cuisine = CuisineType.Italian }
+            };
+        }
+        IEnumerable<Restaurant> IRestaurantData.GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+
+
+}
