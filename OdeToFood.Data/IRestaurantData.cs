9a6a2f9f60ca0b4ec28c8a93fb2f8f22f0d4fe33
Fer Developer <milton.vasquez0726@gmail.com>
2022-01-05 14:00:30 -0500

    Working with SQL and EntityFramework

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -1,7 +1,5 @@
 ﻿using OdeToFood.Core;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,65 +10,10 @@ namespace OdeToFood.Data
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
         Restaurant GetById(int id);
         Restaurant Update(Restaurant updateRestaurant);
-
         Restaurant Add(Restaurant newRestaurant);
-
-
+        Restaurant Delete(int id);
         int commit();
 
     }
-
-    public class InMemoryRestaurantData : IRestaurantData
-    {
-        List<Restaurant> restaurants;
-        public InMemoryRestaurantData()
-        {
-            restaurants = new List<Restaurant>() {
-                new Restaurant { Id = 1, Name = "Milton Pizza",Location = "Colombia", Cuisine = CuisineType.Mexican },
-                new Restaurant { Id = 2, Name = "Milton hamburguer", Location = "Henderson", Cuisine = CuisineType.Indian },
-                new Restaurant { Id = 3, Name = "Milton patapas", Location = "EEUU", Cuisine = CuisineType.Italian }
-            };
-        }
-        IEnumerable<Restaurant> IRestaurantData.GetRestaurantsByName(string name = null)
-        {
-            return from r in restaurants
-                   where string.IsNullOrEmpty(name) || r.Name.Contains(name)
-                   orderby r.Name
-                   select r;
-        }
-
-
-        public Restaurant GetById(int id)
-        {
-
-            return restaurants.SingleOrDefault(r => r.Id == id);
-        }
-
-        public Restaurant Update(Restaurant updateRestaurant)
-        {
-            var restaurant = restaurants.SingleOrDefault(r => r.Id == updateRestaurant.Id);
-        if (restaurant != null)
-            {
-                restaurant.Name = updateRestaurant.Name;
-                restaurant.Location = updateRestaurant.Location;
-                restaurant.Cuisine = updateRestaurant.Cuisine;
-            }
-            return restaurant;
-        }
-
-        public Restaurant Add(Restaurant newRestaurant) {
-            restaurants.Add(newRestaurant);
-            newRestaurant.Id = restaurants.Max(r => r.Id + 1);
-            return newRestaurant;
-
-        }
-
-
-        public int commit() {
-            return 0;
-        }
-
-    }
-
 
 }
